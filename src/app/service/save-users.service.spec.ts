27a7f7eb0f41e6675210c40f0fc73a29ece42274
MayconCarvalho <maycon.arth.carvalho@gmail.com>
2023-11-09 23:59:07 -0300

    fix tests person service

--- src/app/service/save-users.service.spec.ts
+++ src/app/service/save-users.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClientModule } from '@angular/common/http';
 import { SaveUsersService } from './save-users.service';
 
 describe('SaveUsersService', () => {
   let service: SaveUsersService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(SaveUsersService);
   });
 
