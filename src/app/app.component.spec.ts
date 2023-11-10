383f0ef5f9bc98f6bedaf53236dad9ca9fe754f8
MayconCarvalho <maycon.arth.carvalho@gmail.com>
2023-11-09 23:58:41 -0300

    fix tests app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, HttpClientModule],
     }).compileComponents();
   });
 
@@ -24,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, saveUsersFrontend');
+    expect(compiled.querySelector('h2')?.textContent).toContain('Cadastro de Usuários!');
   });
 });
